796608d909a1db8c86fa58c892fce3b86b13aa3d
exnaton18 <exnaton18@gmail.com>
2016-06-02 18:38:19 +0300

    last steps in front

---
+++ www/js/searchAnimate.js
@@ -0,0 +1,8 @@
+function anichange (objName) {
+    if ( $(objName).css('display') == 'none' ) {
+        $(objName).animate({height: 'show'}, 400);
+    } else {
+        $(objName).animate({height: 'hide'}, 200);
+        $(objName).animate({height: 'show'}, 400);
+    }
+}
